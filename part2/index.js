03788a78d116696a776978f25957c02fe63bca07
kedareswar12 <kedareswartiruveedhi@gmail.com>
2026-06-16 20:20:33 +0530

    Started with nodeJs

---
+++ part2/index.js
@@ -0,0 +1,30 @@
+// const { isUtf8 } = require("buffer");
+// const fs = require("fs");
+
+// // console.log(fs);
+
+// const content = fs.readFileSync('text.txt', 'utf-8')
+
+// console.log(content);
+
+const fs = require("fs");
+
+const contents = fs.readFileSync("text.txt", "utf-8");
+
+console.log(contents);
+
+const newFile = fs.writeFileSync(
+  "copy.txt",
+  "I am kedareswar writtent this content by using the writeFileSync",
+  "utf-8",
+);
+// every time overwrite the content
+// so use appendFileSync
+
+fs.appendFileSync(
+  "copy.txt",
+  "\nI am adding the data throgh appendFileSync",
+  "utf-8",
+);
+
+// console.log(newFile);
